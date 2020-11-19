b6a38188a0fed3dac450a730b82a9c19811b4a42
Vladimir Shishkin <shaman.hex@gmail.com>
2020-11-19 19:00:45 +0600

    Add multiple inheritance example

--- Demo/StructAndClasses/StructAndClasses/StructAndClasses.cpp
+++ Demo/StructAndClasses/StructAndClasses/StructAndClasses.cpp
@@ -7,32 +7,46 @@ struct Point
     bool isVisible;
 };
 
+class MyPoint
+{
+public:
+    int X;
+    int Y;
+
+    MyPoint(int x, int y)
+    {
+        this->X = x;
+        this->Y = y;
+    };
+};
+
+template <class T>
 class ListItem
 {
 private:
-    void* lpValue = NULL;
+    T* lpValue = NULL;
     ListItem* lpNext = NULL;
 
 public:
-    ListItem(const ListItem& lpItem)
+    ListItem(const ListItem* lpItem)
     {
         lpValue = lpItem.lpValue;
         lpNext = lpItem.lpNext;
     }
 
-    ListItem(const void* lpValue)
+    ListItem(const T* lpValue)
     {
-        lpValue = lpValue;
+        this->lpValue = (T*) lpValue;
     }
 
-    void* getValue()
+    T* getValue() 
     {
         return this->lpValue;
     }
 
-    void setValue(const void* lpValue)
+    void setValue(const T* lpValue)
     {
-        this->lpValue = (void*) lpValue;
+        this->lpValue = lpValue;
     }
 
     ListItem* getNext()
@@ -46,10 +60,11 @@ public:
     }              
 };
 
+template <class T>
 class List
 {
 private:
-    ListItem* lpFirstItem;
+    ListItem<T>* lpFirstItem;
     int count;
 
 public:
@@ -61,23 +76,24 @@ public:
 
     ~List()
     {
-        ListItem* lpCurItem = this->lpFirstItem;
+        ListItem<T>* lpCurItem = this->lpFirstItem;
         while (lpCurItem != NULL)
         {
-            ListItem* lpNextItem = lpCurItem->getNext();
+            ListItem<T>* lpNextItem = lpCurItem->getNext();
             delete lpCurItem;
             lpCurItem = lpNextItem;
         }
+        this->lpFirstItem = NULL;
     }
 
     int Count()
     {
         return count;
-    };
+    }
 
-    void Add(const void* lpValue)
+    void Add(const T* lpValue)
     {
-        ListItem* lpNewItem = new ListItem(lpValue);
+        ListItem<T>* lpNewItem = new ListItem<T>(lpValue);
 
         if (this->lpFirstItem == NULL)
         {
@@ -85,7 +101,7 @@ public:
         }
         else
         {
-            ListItem* lpLastItem = this->lpFirstItem;
+            ListItem<T>* lpLastItem = this->lpFirstItem;
             while (lpLastItem->getNext() != NULL)
             {
                 lpLastItem = lpLastItem->getNext();
@@ -94,13 +110,187 @@ public:
         }
         count++;  
     }
+
+    void DeleteLast()
+    {
+        if (this->lpFirstItem != NULL)
+        {
+            ListItem<T>* lpPrevItem = NULL;
+            ListItem<T>* lpLastItem = this->lpFirstItem;
+            while (lpLastItem->getNext() != NULL)
+            {
+                lpPrevItem = lpLastItem;
+                lpLastItem = lpLastItem->getNext();
+            }
+            lpPrevItem->setNext(NULL);
+            delete lpLastItem;
+        }
+    }
+
+    T* GetItemByIndex(int index)
+    {
+        ListItem<T>* lpCurItem = this->lpFirstItem;
+        for (int i = 0; i < index; i++)
+        {
+            if (lpCurItem == NULL)
+            {
+                return NULL;
+            }
+            lpCurItem = lpCurItem->getNext();
+        }
+        return lpCurItem->getValue();
+    }
+
+    List* operator += (const T* item)
+    {
+        this->Add(item);
+        return this;
+    }
+
+    List* operator << (const T* item)
+    {
+        this->Add(item);
+        return this;
+    }
+
+    T* operator[] (int index)
+    {
+        return this->GetItemByIndex(index);
+    }
+};
+
+template <class T>
+class Stack : private List<T>
+{
+public:
+    Stack()
+    {
+    }
+
+    void Push(T* item)
+    {
+        this->Add(item);
+    }
+
+    T* Pull()
+    {
+        T* value = this->GetItemByIndex(this->Count() - 1);
+        this->DeleteLast();
+        return value;
+    }
+};
+
+class Person
+{
+public:
+    char* lpszName;
+
+    Person()
+    {
+        lpszName = NULL;
+        if (lpszName == NULL)
+        {
+            std::cout << "Create Persone() -> allocate memory for Name\n";
+            lpszName = new char[500];
+        }
+        std::cout << "Create Persone()\n";
+    }
+
+    ~Person()
+    {
+        std::cout << "Delete Persone()\n";
+        if (lpszName != NULL)
+        {
+            std::cout << "Delete Persone() -> free memory for Name\n";
+            delete lpszName;
+            lpszName = NULL;
+        }
+    }
+
+    void PrintName()
+    {
+        std::cout << "Name: " << this->lpszName << "\n";
+    }
+
+};
+
+class Student : public Person
+{
+public:
+    Student()
+    {
+        sprintf_s(lpszName, 500, "Student");
+        std::cout << "Create Student()\n";
+    }
+
+    ~Student()
+    {
+        std::cout << "Delete Student()\n";
+    }
+};
+
+class Doctor : public Person
+{
+public:
+    Doctor()
+    {
+        sprintf_s(lpszName, 500, "Doctor");
+        std::cout << "Create Doctor()\n";
+    }
+
+    ~Doctor()
+    {
+        std::cout << "Delete Doctor()\n";
+    }
+};
+
+class StudentDoctor : public Student, public Doctor
+{
+public:
+    StudentDoctor()
+    {
+        std::cout << "Create StudentDoctor()\n";
+    }
+
+    ~StudentDoctor()
+    {
+        std::cout << "Delete StudentDoctor()\n";
+    }
 };
 
 int main()
 {
-    int x = 10;
-    List list;     
-    list.Add(&x);
-    list.Add(&x);
-    std::cout << "Item number " << list.Count() << "\n";
+    List<MyPoint> PointsList;
+    PointsList.Add(new MyPoint(10, 100));
+    PointsList.Add(new MyPoint(20, 200));
+    PointsList.Add(new MyPoint(30, 300));
+
+    PointsList += new MyPoint(10, 10);
+    PointsList << new MyPoint(10, 10);
+
+    MyPoint* p = PointsList.GetItemByIndex(0);
+    p = PointsList[0];
+
+    std::cout << "Item number " << PointsList.Count() << "\n";
+    std::cout << "Item[0] = {X = " << p->X << "; Y = " << p->Y << "}\n";
+
+    std::cout << "\n";
+    std::cout << "\n";
+
+    Student* stud = new Student();
+    stud->PrintName();
+    delete stud;
+
+    std::cout << "\n";
+
+    Doctor* doc = new Doctor();
+    doc->PrintName();
+    delete doc;
+
+    std::cout << "\n";
+
+    StudentDoctor* x = new StudentDoctor();
+    ((Doctor*) x)->PrintName();
+    delete x;
+
 }
